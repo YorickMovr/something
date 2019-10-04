42ac05eef8fb1ffa155439e6290bcef3fa938a2a
YorickMovr <yoricksamovar@gmail.com>
2019-10-04 08:35:01 +0300

    f

---
+++ пробы/app.js
@@ -0,0 +1,13 @@
+class Gallery{
+
+}
+
+const myGallery = new Gallery(
+                        document.getElementById('container')
+                        )
+
+/* Опциональное задание - реализовать такие методы */
+
+// myGallery.show(2);
+// myGallery.next();
+// myGallery.prev();
