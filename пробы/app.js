b3c3d05606fa7dd3fd16f50109f036002023141a
YorickMovr <yoricksamovar@gmail.com>
2019-10-04 09:43:12 +0300

    d

--- пробы/app.js
+++ пробы/app.js
@@ -1,6 +1,45 @@
+const SLIDE_HIDDEN = 'slide_hiden';
+const SLIDE_SHOWN = 'slide_shown';
+
 class Gallery{
+    constructor(slides){
+        this.slides = slides;
+        this.currentSlide = 0;
+        this.doSlider();
+    };
+
+    get images (){
+        return this.slides.children;
+    };
+
+    get slidesCount (){
+        return this.images.length;
+    }
+
+};
+
+Gallery.prototype.doSlider = function(){
+
+};
+
+Gallery.prototype.CreateSlides = function(){
 
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 const myGallery = new Gallery(
                         document.getElementById('container')
