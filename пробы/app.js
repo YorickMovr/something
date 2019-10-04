6b80c089e642e49eb98b916aa6dfa0203dd62bd3
YorickMovr <yoricksamovar@gmail.com>
2019-10-04 12:38:36 +0300

    d

--- пробы/app.js
+++ пробы/app.js
@@ -1,10 +1,8 @@
-const SLIDE_HIDDEN = 'slide_hiden';
-const SLIDE_SHOWN = 'slide_shown';
-
 class Gallery{
     constructor(slides){
         this.slides = slides;
         this.currentSlide = 0;
+        this.createSlides();
         this.doSlider();
     };
 
@@ -19,11 +17,32 @@ class Gallery{
 };
 
 Gallery.prototype.doSlider = function(){
-
+    // let slideInterval = setInterval(() => this.showNextSlide(), 3000);
+    previous.onclick = this.showPrevSlide;
+    document.addEventListener
 };
 
-Gallery.prototype.CreateSlides = function(){
+Gallery.prototype.createSlides = function(){
+    this.slides.className = 'slides';
+    this.images[0].className = 'slide showing';
+    for(i=1; i < this.slidesCount; i++){
+        this.images[i].className = 'slide';
+    }
+};
 
+Gallery.prototype.showNextSlide = function(){
+    this.images[this.currentSlide].className = 'slide';
+    this.currentSlide = (this.currentSlide +1) % this.slidesCount;
+    this.images[this.currentSlide].className = 'slide showing';
+}
+
+Gallery.prototype.showPrevSlide = function(){
+    this.images[this.currentSlide].className = 'slide';
+    this.currentSlide = (this.currentSlide - 1);
+    if(this.currentSlide <0){
+        this.currentSlide == 5;
+    }
+    this.images[this.currentSlide].className = 'slide showing';
 }
 
 
